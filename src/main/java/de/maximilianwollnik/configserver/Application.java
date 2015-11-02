df07c68a3dc75240b02f0a02a3029b0df19c4267
maximilianwollnik <maximilianwollnik@users.noreply.github.com>
2015-11-02 22:59:45 +0100

    add: project setup
    - initial revision

---
+++ src/main/java/de/maximilianwollnik/configserver/Application.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015, Maximilian Wollnik, All rights reserved.
+ */
+package de.maximilianwollnik.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configserver for my homepage
+ * @author maximilian
+ * @version 1.0
+ * @since 0.1.0
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
+public class Application {
+
+  /**
+   * The main method.
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
