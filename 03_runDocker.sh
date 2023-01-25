4b9e5374e9a16933913148024f85f09e390f9a73
maximilianwollnik <maximilian@maximilianwollnik.de>
2023-01-25 21:07:27 +0100

    restoring image after docker update

--- 03_runDocker.sh
+++ 03_runDocker.sh
@@ -2,4 +2,4 @@ function prop {
     grep "${1}" gradle.properties|cut -d'=' -f2
 }
 
-docker run -d --name configserver --network spring maximilianwollnik/configserver:$(prop 'version')
+docker run -d --restart unless-stopped --name configserver --network spring maximilianwollnik/configserver:$(prop 'version')
